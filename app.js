6554e7e2d0b929ef3b57cc20e998b2013a101b15
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-10-15 12:45:08 +0900

    fix: APP ENTRY - REQUIRE CONFIG

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const loaders = require ('./loaders');
 const express = require('express');
+const {
+    PORT
+} = require('./config');
 
 const startServer = async () => {
     const app = express();
     await loaders.init({ expressApp : app});
 
-    app.listen(process.env.PORT, err =>{
+    app.listen(PORT, err =>{
         if (err) {
             console.error(err);
             return;
         }
-        console.log('Server Ready');
+        console.log(`Server Ready`);
     });
 };
 
