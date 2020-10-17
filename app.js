595c868fdd12c30fe5c17e88af05b264f167ec6a
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-10-17 19:06:19 +0900

    feat: EXPRESS ROUTER - API

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const {
     PORT
 } = require('./config');
 
+const indexRouter = require('./api/index');
+
 const startServer = async () => {
     const app = express();
     await loaders.init({ expressApp : app});
+
+    /** router */
+    
+    app.use('/', indexRouter);
 
     app.listen(PORT, err =>{
         if (err) {
